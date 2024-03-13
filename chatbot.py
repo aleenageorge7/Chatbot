6775b29d03b8a21916bf68bb708be77a66c0d76a
aleenageorge7 <aleenasalomy26@gmail.com>
2024-03-13 22:40:32 +0530

    Added chatbot

---
+++ chatbot.py
@@ -0,0 +1,69 @@
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+import requests
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open(r'C:\Users\ALEENA\Desktop\project\New folder\intents.json').read())
+
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+model = load_model('chatbot_model.h5')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words] 
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i,word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent':classes[r[0]],'probability':str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def weather(city):
+    url = f"https://api.weatherapi.com/v1/current.json?key=3ae3dea5a8864fd6bf2140701230606&q={city}"
+    r = requests.get(url)
+    weather_data = json.loads(r.text)
+    temperature = weather_data['current']['temp_c']
+    return f"The Temperature of {city} is {temperature}"
+
+print("GO! Bot is running")
+
+while True:
+    message = input("")
+    if message.startswith("What is the weather in "):
+        city = message[23:]
+        print(weather(city))
+    else:
+        ints = predict_class(message)
+        res = get_response(ints, intents)
+        print(res)
